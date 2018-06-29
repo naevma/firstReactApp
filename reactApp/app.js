c3de849781421ad2923a3a11df43eb8d6bd01a0e
Nadine <naevma@umich.edu>
2018-06-29 16:00:09 -0700

    added static list, with x's, and can cross out if completed

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p> yooo</p>,
+var dummyData = [{taskText: "get groceries", completed: true }, { taskText: "get oil changed", completed: false }, { taskText: "send thank you letters", completed: false }];
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ul>
+        {dummyData.map((element) => <li><button>X</button>{element.completed ? <strike>{element.taskText}</strike> : element.taskText}
+        </li>)}
+
+      </ul>
+    )
+  }
+}
+
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <div>
+      <input type="text" placeholder="task" />
+      <button type="submit">Add todo</button>
+      </div>
+    )
+  }
+}
+
+class TodoApp extends React.Component {
+  render(){
+    return(
+      <div>
+      <InputLine/>
+      <TodoList/>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<TodoApp />,
 document.getElementById('root'));
