9c57df05912449fc81794500cc51868238bd6578
Nadine <naevma@umich.edu>
2018-06-29 14:48:46 -0700

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './reactApp/app.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+          }
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
